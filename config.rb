b7b06f18bc14ce5ddc358865b6643f25f9c306de
Adam Sheehan <a.t.sheehan@gmail.com>
2016-09-17 13:22:20 -0400

    Add custom markdown renderer to include asides

--- config.rb
+++ config.rb
@@ -1,3 +1,30 @@
+MARKDOWN_OPTIONS = {
+  fenced_code_blocks: true,
+  smartypants: true,
+  no_intra_emphasis: true,
+  autolink: true,
+  disable_indented_code_blocks: true
+}
+
+class CustomMarkdownRenderer < Redcarpet::Render::HTML
+  def initialize(options = {})
+    super(options)
+  end
+
+  def preprocess(document)
+    renderer = Redcarpet::Markdown.new(self, options = MARKDOWN_OPTIONS)
+    aside_blocks(document, renderer)
+  end
+
+  private
+
+  def aside_blocks(document, renderer)
+    document.gsub(/^\[\[aside\n((.|\n)*?)^aside\]\]$/) do |foo|
+      "<aside>#{renderer.render($1)}</aside>"
+    end
+  end
+end
+
 helpers do
   require "date"
 
@@ -14,7 +41,7 @@ set :css_dir, "css"
 set :images_dir, "images"
 set :build_dir, "build/foobarium"
 set :markdown_engine, :redcarpet
-set :markdown, fenced_code_blocks: true, smartypants: true
+set :markdown, MARKDOWN_OPTIONS.merge(renderer: CustomMarkdownRenderer)
 
 configure :build do
   activate :minify_css
